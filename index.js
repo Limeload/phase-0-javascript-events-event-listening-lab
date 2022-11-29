86a87865700341245cfb90bff173e0d380e9716a
Shraddha Rao <shraddharaom@gmail.com>
2022-11-29 08:45:53 -0600

    commit tree f8b7f4f062d19adcb45582c7e4cb1cdd035c04b8

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input = document.getElementById("button")
+
 function addingEventListener() {
+    alert('I am poked');
+    return input.addEventListener("click", addingEventListener);
+
 }
+
